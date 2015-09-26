c042881fd7a724910f5986b67d8a92a731a98711
TonyDevNyc <tonydevgroupny@gmail.com>
2015-09-26 18:16:24 -0400

    remove test email service

--- traks-parent/traks-service/src/main/java/com/target/trak/system/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.target.trak.system.service;
-
-import java.util.List;
-
-public interface EmailService {
-
-	public void sendEmailMessage(List<String> recipients, String subject, String message);
-	
-}
